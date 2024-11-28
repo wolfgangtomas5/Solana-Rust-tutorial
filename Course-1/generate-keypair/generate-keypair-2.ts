d1fb2e6de73cfaeb6c059dce267b9f83dbda8d2a
foxier25 <wolfgangtomas5@gmail.com>
2024-11-28 07:39:34 -0600

    course1-lesson2

---
+++ Course-1/generate-keypair/generate-keypair-2.ts
@@ -0,0 +1,9 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+ 
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+ 
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`, keypair
+);
